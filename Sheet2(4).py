eb4fe2169d3771ec518fbb472380c0843fe324d3
Vignesh Gupta <vigneshgupta098@gmail.com>
2025-08-19 01:26:47 +0530

    final commit

--- Sheet2(4).py
+++ Sheet2(4).py
@@ -2,7 +2,8 @@
 # 4. Write a program to print all odd numbers from 1 to N, where you have to take N as input 
 # from user. 
 
-N = int(input("Enter a number: "))
-for x in range(1, N+1):
-    if (x%3==0):
-        print(x,end=" ")
+N = int(input("Enter a number:\n"))
+print("Odd no.from 1 to", N, "are:")
+for i in range(1, N+1):
+    if i % 2 != 0:   
+        print(i, end=" ")
