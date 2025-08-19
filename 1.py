eaccc23da792afef7549510ff0f1f3c99acee14f
VigneshGupta098 <vigneshgupta098@gmail.com>
2025-08-19 11:30:32 +0530

    1change

--- 1.py
+++ 1.py
@@ -2,6 +2,8 @@
 
 Number = int(input("enter the number:\n"))
 if Number%7==0:
-    print("Yes,it is divisible by 7")
+    print("Yes,it is divide by 7")
 else:
     print("NO,it is not divisible by 7")
+
+    # hello
